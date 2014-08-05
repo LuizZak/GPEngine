18c56562f24f763c1f2f2bccaed8688fab2b6625
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2014-08-05 12:45:06 -0300

    Adding private internal methods on GPEntity to manage components

--- GPEngine/GPEntity.swift
+++ GPEngine/GPEntity.swift
@@ -34,19 +34,46 @@ class GPEntity: NSObject
     }
     
     // Adds the given component to this entity
-    func addComponent(component : GPComponent)
+    func addComponent(component: GPComponent)
+    {
+        self.internalAddComponent(component);
+        
+        // Notify the game scene this entity has been modified
+        self.gameScene?.entityModified(self);
+    }
+    
+    // Adds an array of components to this entity
+    func addComponents(components: [GPComponent])
+    {
+        self.components += components;
+        
+        // Notify the game scene this entity has been modified
+        self.gameScene?.entityModified(self);
+    }
+    
+    // Removes a component from this entity
+    func removeComponent(component: GPComponent)
+    {
+        self.internalRemoveComponent(component);
+        
+        // Notify the game scene this entity has been modified
+        self.gameScene?.entityModified(self);
+    }
+    
+    // Intenral method that adds a components to this entity
+    private func internalAddComponent(component: GPComponent)
     {
         self.components += component;
     }
     
-    // Removes a component from this entity
-    func removeComponent(component : GPComponent)
+    // Internal method that removes components from this entity
+    private func internalRemoveComponent(component: GPComponent)
     {
-        self.components.remove(component);
+        self.components -= component;
     }
     
     // Returns whether the entity has the given component type inside of it
-    func hasComponentType(type : AnyClass) -> Bool
+    func hasComponentType(type: AnyClass) -> Bool
     {
         for comp in self.components
         {
