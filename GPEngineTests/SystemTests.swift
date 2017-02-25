24fdd9fdb8816114e10393c258001ca4cbdb17c9
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2017-02-25 15:02:07 -0300

    Adjusting unit tests

--- GPEngineTests/SystemTests.swift
+++ GPEngineTests/SystemTests.swift
@@ -12,25 +12,18 @@ import XCTest
 import SpriteKit
 
 class SystemTests: XCTestCase {
-    var view: SKView?
     var game: Game = Game()
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
         
-        view = SKView()
         game = Game()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-        
-        
-        view = nil
     }
-
+    
     func testAddSystem() {
         let system = CustomSystem()
         
